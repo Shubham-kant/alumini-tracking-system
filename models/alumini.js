19f8c8101a5b6e747d7775e91c73e85a1eb20e7c
Shubham-Kant <pro.shubham1999@gmail.com>
2020-02-03 14:22:29 +0530

    added alumni schema

---
+++ models/alumini.js
@@ -0,0 +1,57 @@
+const mongoose=require('mongoose');
+const alumniSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    collegename:{
+        type:String,
+        required:true
+    },
+    branch:{
+        type:String,
+        required:true
+        
+
+    },
+    address:{
+        type:String,
+        required:true
+
+    },
+    phone:{
+        type:Number,
+        required:true
+
+    },
+    country:{
+        type:String,
+        required:true
+    },
+    avatar:{
+
+    },
+    sex:{
+        type:String,
+        required:true
+
+    },
+    batch:{
+        type:Number,
+        required:true
+
+    },
+    dob:{
+        type:String,
+        required:true
+    }
+},{
+    timestamps:true
+});
+const Alumni=mongoose.model('Alumni',alumniSchema);
+module.exports=Alumni;
