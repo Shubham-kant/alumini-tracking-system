adab62d6875f419133e305fab40defd515b0b41c
Shubham-Kant <pro.shubham1999@gmail.com>
2020-02-03 13:38:46 +0530

    change db name

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-mongoose.connect('mongodb://localhost/codeial_development');
+mongoose.connect('mongodb://localhost/alumini_track_development');
 
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,'error in connecting to Mongodb'));
